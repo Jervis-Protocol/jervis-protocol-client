8822db93367e96ec916204594a24fb9296c3f09f
HanHyun <han911116@gmail.com>
2023-09-15 16:05:52 +0900

    fix: metamask connection error

--- src/service/modal.connect.service.tsx
+++ src/service/modal.connect.service.tsx
@@ -43,7 +43,8 @@ async function requestAccount() {
 }
 
 const onConnectMetamask = async (networkId: number): Promise<IWalletAuth> => {
-    await requestAccount();
+    const account = await requestAccount();
+    console.log(account);
     // @ts-ignore
     await provider().enable();
     // @ts-ignore
