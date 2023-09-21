7f2a10c42870149700ff68da59191f0cd93921d7
HanHyun <han911116@gmail.com>
2023-09-21 10:28:29 +0900

    feat: update web3 to ethers

--- src/service/summary.service.tsx
+++ src/service/summary.service.tsx
@@ -8,12 +8,16 @@ import {IFundingFactory} from "../type/_data/contract.type";
 import bigDecimal from "js-big-decimal";
 import {ABI} from '../abis/factory.abi';
 import {provider, requestAccount} from "../helper/wallet.provider.ts";
+import { ethers } from "ethers";
+
 declare global {
     interface Window {
         ethereum: any;
         klaytn: any;
     }
 }
+
+const CREATE_EVENT = "0x96b5b9b8a7193304150caccf9b80d150675fa3d6af57761d8d8ef1d6f9a1a909";
 
 export const onDataProcessing = (funding: IInputFunding): Promise<IInputFunding> => post('/api/funding/processing', funding);
 export const onFundingCreate = (funding: IInputFunding): Promise<IFunding> => post('/api/funding/create', funding);
@@ -50,14 +54,24 @@ const setNFTValud = (funding: IInputFunding) => {
 const onDeployBlockChain = async (funding: IInputFunding, contractInfo: IFundingFactory) => {
     await provider().enable();
     console.log("ContractInfo: ", contractInfo);
-    const web3 = new Web3(provider());
-    const account = await requestAccount();
-    const factory = new web3.eth.Contract(ABI as Array<AbiItem>, contractInfo.address);
-    const transaction = await setTransaction(funding, factory);
-    const transactionOptions = getTransactionOptions(contractInfo.networkId, contractInfo.address, account);
-    const receipts = await transaction.send(transactionOptions);
-    return receipts!.events.Create.returnValues.addr
+
+    const eProvider = new ethers.BrowserProvider(provider());
+    const signer = await eProvider.getSigner();
+    const contract = new ethers.Contract(contractInfo.address, ABI, signer);
+    const transaction = await contract.createFunding(funding.goal, new Date(funding.sdate!).getTime() / 1000, new Date(funding.edate!).getTime() / 1000, getNFTPrice(funding.nfts!), getNFTAmount(funding.nfts!), getNFTURI(funding.nfts!), funding.title, funding.symbol!);
+    // const transaction = await setTransaction(funding, factory);
+    // const web3 = new Web3(provider());
+    console.log("transaction: ", transaction);
+    const receipts = await transaction.wait();
+    console.log("receipt: ", receipts);
+    const fundingInterface = new ethers.Interface(ABI);
+
+    const eventLog = receipts.logs?.find((log: any) => log.topics[0] === CREATE_EVENT);
+    const log = fundingInterface.parseLog(eventLog!);
+    console.log("log: ", log);
+    return log!.args[1];
 }
+
 const setTransaction = async (funding: IInputFunding, factory: Contract) => await factory.methods.createFunding(funding.goal, new Date(funding.sdate!).getTime() / 1000, new Date(funding.edate!).getTime() / 1000, getNFTPrice(funding.nfts!), getNFTAmount(funding.nfts!), getNFTURI(funding.nfts!), funding.title, funding.symbol!);
 const getNFTPrice = (nfts: Array<IInputNFT>): Array<string> => nfts.map(nft => nft.price!.toString());
 const getNFTAmount = (nfts: Array<IInputNFT>): Array<number> => nfts.map(nft => nft.max!);
@@ -76,7 +90,7 @@ export const getTransactionOptions = (networkId: number, to: string, from?: stri
         }
         default:
             return {
-                gas: 8000000,
+                // gas: 8000000,
                 value: 0,
                 to: to,
                 from: from
