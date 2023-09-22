92e657fd9f538346c0aed1aef49850120e478e4b
HanHyun <han911116@gmail.com>
2023-09-22 11:32:39 +0900

    feat: Add Profile Page User Address Field

--- src/_containers/profile/profile.banner.tsx
+++ src/_containers/profile/profile.banner.tsx
@@ -9,9 +9,10 @@ const ProfileBannerComponent = () => {
         <div className="row my-7" data-aos="fade-up" data-aos-delay="200">
             <img src={user!.image} alt="profile-img"
                  className="col-md-3 col-12 border border-5 rounded-circle size-10 p-0"/>
-            <div className="col-md-9 col-12 mt-md-10 mt-4">
+            <div className="col-md-9 col-12 mt-md-10 mt-2">
                 <h4 className="col-1 text-center text-uppercase">{user!.nickname}</h4>
-                { auth === true && <div className="col-2 text-start"><Link to={"/edit"} state={{user: user!}} className="btn btn-sm text-secondary height-30 py-0 border-bottom border-3 border-dark">Edit</Link></div> }
+                <small className="col-1 text-center text-uppercase text-muted">{user!.address}</small>
+                { auth === true && <div className="col-2 text-start mt-2"><Link to={"/edit"} state={{user: user!}} className="btn btn-sm text-secondary height-30 py-0 border-bottom border-3 border-dark">Edit</Link></div> }
             </div>
         </div>
     )
